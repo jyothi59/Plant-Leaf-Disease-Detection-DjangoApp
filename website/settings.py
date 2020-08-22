be81668093c58d81b04e13fdacd9453fc8781fbc
jyothi59 <jyothigelli@gmail.com>
2020-08-22 16:26:38 +0530

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = 's2+sndk^9-7t@tj4!wd)yv2@w9%n8a0rc#4eqgzfmjfrh&b6dv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['pldd-django.herokuapp.com','12.0.0.1']
+ALLOWED_HOSTS = ['pldd-django.herokuapp.com','127.0.0.1']
 
-
+#'pldd-django.herokuapp.com','127.0.0.1','localhost'
 # Application definition
 
 INSTALLED_APPS = [
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIR=os.path.join(BASE_DIR,'static')
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
