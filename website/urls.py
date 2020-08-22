f175b53273d603b800a4d443e867a76b0b3de578
jyothi59 <jyothigelli@gmail.com>
2020-08-22 16:27:11 +0530

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -19,6 +19,8 @@ from django.conf.urls import url
 from first import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import static
 
 
 urlpatterns = [
@@ -29,4 +31,7 @@ urlpatterns = [
     url('index',views.index,name="homepage"),
 ]
 
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+#static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
